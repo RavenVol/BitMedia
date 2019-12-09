d28d55c663b81b778448e7f0dc50674243a798b5
RavenVol <r.volianiuk@gmail.com>
2019-12-09 09:20:22 +0200

    Updates

---
+++ precache-manifest.ab4ef8606ae291bac8063ccbddac885a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec886025bf2e9c22df80aaf0ceda4f3",
+    "url": "/BitMedia/index.html"
+  },
+  {
+    "revision": "a2d1e4f79b1e998ac81c",
+    "url": "/BitMedia/static/css/main.6077ff9e.chunk.css"
+  },
+  {
+    "revision": "ceea6bffde09d32b67f3",
+    "url": "/BitMedia/static/js/2.ebe735af.chunk.js"
+  },
+  {
+    "revision": "a2d1e4f79b1e998ac81c",
+    "url": "/BitMedia/static/js/main.d47d3fde.chunk.js"
+  },
+  {
+    "revision": "79cca6e1c4bf455873f5",
+    "url": "/BitMedia/static/js/runtime~main.78365743.js"
+  }
+]);
